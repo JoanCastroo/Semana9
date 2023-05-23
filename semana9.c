a677b9dd31a517a092fc61197b8faf31a6510cab
JoanCastroo <131194146+JoanCastroo@users.noreply.github.com>
2023-05-23 12:51:29 -0500

    Update semana9.c

--- semana9.c
+++ semana9.c
@@ -7,7 +7,39 @@ void llenarMatriz(int **matriz, int filas, int columnas) {
     
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < columnas; j++) {
-            matriz[i][j] = rand() % 100 + 1;  
+            matriz[i][j] = rand() % 100 + 1;  // Generar número aleatorio entre 1 y 100
         }
     }
 }
+
+void imprimirMatriz(int **matriz, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void calcularTranspuesta(int **matriz, int **transpuesta, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            transpuesta[j][i] = matriz[i][j];
+        }
+    }
+}
+
+int main() {
+    int filas, columnas;
+    
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+    
+    // Crear matriz original
+    int **matriz = (int **)malloc(filas * sizeof(int *));
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = (int *)malloc(columnas * sizeof(int));
+    }
+    
